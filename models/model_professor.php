69a92e5737caf7eeed6032785749b07315e624b0
christianmurata <chris_murata@hotmail.com>
2017-08-25 16:10:32 -0300

    novo

---
+++ models/model_professor.php
@@ -0,0 +1,14 @@
+<?php 
+    include_once("suporte.php");
+    include_once("servico.php");
+    include_once("entidades.php");
+
+    $Model_professor = new Model_professor();
+
+    class Model_professor{
+        static function teste(){
+            session_start();
+            
+        }
+    }
+?>
