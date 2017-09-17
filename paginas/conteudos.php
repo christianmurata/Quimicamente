82ea49ecb2c9ddd1574aefc365a06a6012d35e94
christianmurata <chris_murata@hotmail.com>
2017-09-17 02:22:45 -0300

    arquivos adicionados

---
+++ paginas/conteudos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/css_conteudos.css">
+	<link rel="stylesheet" href="../css/animate.css">
+	<link rel="stylesheet" href="../css/hover.css">
+	<script src="../js/jquery.min.js"></script>
+	<script src="../js/java.js"></script>
+    <title>Quimicamente | Curso</title>
+	<link rel="shortcut icon" href="../imagens/logo.ico">
+</head>
+<body>
+    <header>
+        <?php include 'templates/navbar.php'; ?>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
